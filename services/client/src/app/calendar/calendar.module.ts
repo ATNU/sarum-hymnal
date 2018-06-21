6f93d4ee2717fe9584074c6e70c600a719a6e706
Mark Turner <mark.turner@ncl.ac.uk>
2018-06-21 15:02:37 +0100

    Added date picker for choosing hymns by date

--- services/client/src/app/calendar/calendar.module.ts
+++ services/client/src/app/calendar/calendar.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule, MatDatepickerModule, MatInputModule, MatFormFieldModule } from '@angular/material';
 import { CalendarComponent } from './calendar.component';
+import { MomentDateModule } from '@angular/material-moment-adapter';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatIconModule
+    FormsModule,
+    ReactiveFormsModule,
+    MomentDateModule,
+    MomentModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   declarations: [
     CalendarComponent
