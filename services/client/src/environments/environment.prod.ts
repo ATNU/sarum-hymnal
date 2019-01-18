377efc7e11e73e9ae47cffcefdd629a8ca05b612
Mark Turner <nmdt3@roving-10-70-39-170.ncl.ac.uk>
2019-01-18 07:59:53 +0000

    Fixed server URL

--- services/client/src/environments/environment.prod.ts
+++ services/client/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'http://localhost:2200',
+  serverUrl: 'http://sarum-hymnal.atnu.di-projects.net',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
