5ff630c3f141d4c5198afadd6b504fcad25cb273
andywhardy <andywhardy@gmail.com>
2019-07-23 15:00:05 +0100

    added circle ci api token

--- services/cantaloupe/runBuild.sh
+++ services/cantaloupe/runBuild.sh
@@ -6,4 +6,3 @@ ls
 docker run -it --rm --name cantaloupeBuild -v "$PWD/.cache/cantaloupe":/usr/src/cantaloupe -v $PWD/.cache/mvn:/root/.m2 -w /usr/src/cantaloupe maven:3.2-jdk-8 mvn clean package -DskipTests
 mv .cache/cantaloupe/target/cantaloupe-4.0-SNAPSHOT.war services/cantaloupe/cantaloupe.war
 rm -rf .cache/cantaloupe
-
